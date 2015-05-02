8de5091cc98ab000ce507ff93ab027937f3066e3
Loren <neutronstar@gmail.com>
2015-05-02 02:26:03 -0700

    looks sorta cool

--- shapes.js
+++ shapes.js
@@ -64,13 +64,14 @@ function Shape(params) {
   shape.y = params.y || 0 ;
   shape.size = params.size || 0 ;
   shape.edges = params.edges || makeCyclicEdges(params.size) ;
+  shape.color = params.color || '#000';
   
   shape.d = function $d() {
     var str, i;
     
     str = '';
 
-    for (i = 0; i < shape.edges.length - 1; i++) {
+    for (i = 0; i < shape.edges.length; i++) {
       if (i === 0) { 
         str += 'M' + shape.edges[i].v0.x + ' ' + shape.edges[i].v0.y;
       }
@@ -79,6 +80,33 @@ function Shape(params) {
 
     return str;
   };
+
+  function bezierValues(edge, i) {
+    var str, dist, angle, magnitude;
+    
+    dist = Math.sqrt( Math.pow(edge.v1.x - edge.v0.x, 2) +
+                      Math.pow(edge.v1.y - edge.v0.y, 2)
+    );
+
+    a0 = Math.atan( edge.v0.y / edge.v0.x );
+    a1 = Math.PI + Math.atan( edge.v1.y / edge.v1.x );
+
+    magnitude = 0.3 * dist;
+    
+
+    return 'M' + edge.v0.x + ' ' + edge.v0.y + ' C ' +
+          ( edge.v0.x + (Math.sin(a0) * magnitude) ) + ' ' +
+          ( edge.v0.y + (Math.cos(a0) * magnitude) ) + ', ' +
+          ( edge.v1.x + (Math.sin(a1) * magnitude) ) + ' ' +
+          ( edge.v1.y + (Math.cos(a1) * magnitude) ) + ', ' +
+          edge.v1.x + ' ' + edge.v1.y
+
+    ;
+
+
+  }
+
+  shape.dC = bezierValues ;
 
   shape.setPositions = function $setPositions() {
     // $v: vertex
@@ -157,20 +185,59 @@ function View(params) {
         })
     ;
 
-    for (i = 0; i < shape.edges.length; i++) {
-      d3Shape.append('line')
+    // for (i = 0; i < shape.edges.length; i++) {
+    //   d3Shape.append('line')
+    //     .attr({
+    //       x1: shape.edges[i].v0.x,
+    //       y1: shape.edges[i].v0.y,
+    //       x2: shape.edges[i].v1.x,
+    //       y2: shape.edges[i].v1.y
+    //     })
+    //     .style({
+    //       stroke: 'black',
+    //       'stroke-width': 1
+    //     })
+    //   ;
+    // }
+
+    // for (i = 0; i < shape.edges.length; i++) {
+    d3Shape.append('path')
         .attr({
-          x1: shape.edges[i].v0.x,
-          y1: shape.edges[i].v0.y,
-          x2: shape.edges[i].v1.x,
-          y2: shape.edges[i].v1.y
+          d: shape.d(shape.edges[i])
         })
         .style({
-          stroke: 'black',
-          'stroke-width': 1
+          fill: shape.color,
+          stroke: shape.color,
+          'fill-opacity': 0.03,
+          'stroke-opacity': 0.1
         })
-      ;
+    ;
+    //}
+  };
+
+  view.blah = function(params) {
+    // var primes = [3,5,7,11,13,17,19,23,29];
+    var primes = [3,6,9];
+    var s = [4, 6, 8, 10];
+
+    function a(z) {
+    z.forEach(function(el, i) { 
+      params.size = el;
+      params.i = i;
+      view.drawShape(params);
+    });
     }
+
+    a(primes);
+    params.x += 300;
+    a(s);
+    
+
+    // for (i = 0; i < 10; i++) {
+    //   params.size = i + 3;
+    //   params.i = i;
+    //   view.drawShape(params);
+    // }
   };
   
   return view;
